92460a075caaf020a758df0774179ad56c15ba98
ohjihun <gown10195@gmail.com>
2020-03-26 12:34:35 +0900

    java spring-boot project (category added & before filtering)

---
+++ eatgo/eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/CategoryController.java
@@ -0,0 +1,41 @@
+package kr.co.fastcampus.eatgo.interfaces;
+
+import kr.co.fastcampus.eatgo.application.CategoryService;
+import kr.co.fastcampus.eatgo.domain.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+
+    @GetMapping("/categories")
+    public List<Category> list(){
+        List<Category> categories = categoryService.getCategories();
+
+        return categories;
+    }
+
+    @PostMapping("/categories")
+    public ResponseEntity<?> create(
+            @Valid @RequestBody Category resource
+    ) throws URISyntaxException {
+
+        Category category = categoryService.addCategory(resource.getName());
+
+        URI location = new URI("/categories/" + category.getId());
+        return ResponseEntity.created(location).body("{}");
+    }
+}
---
+++ eatgo/eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/Category.java
@@ -0,0 +1,24 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity     //메뉴아이템 레포에 대한 구현이 없이도 JPA로 구현가능함
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Setter
+    private Long restaurantId;
+
+    private String name;
+}
